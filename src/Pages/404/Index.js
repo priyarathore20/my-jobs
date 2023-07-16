62a97e72c1efed835d9a86e76fbc9d0f88e11219
priya rathore <rathorepriya1705@gmail.com>
2023-07-16 18:44:07 +0530

    next commit

--- src/Pages/404/Index.js
+++ src/Pages/404/Index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import {MdError} from  'react-icons/md'
+import "./styles.css"
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return <div className='not-found'>
+  {/* <MdError className='error-icon' />
+  <h1 className='error-msg'>Page not found !</h1> */}
+  <img className='error-img' src="https://www.seekahost.com/wp-content/uploads/2017/11/404-page-not-found.jpg" alt="404" />
+  </div>;
 };
 
 export default NotFound;
